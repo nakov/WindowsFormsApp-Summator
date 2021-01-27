2ecad35ac7370ddf728cf7c6cc4cd050d8a8d640
Svetlin Nakov <nakov@users.noreply.github.com>
2021-01-28 01:56:06 +0200

    Added the source code

---
+++ FormSummator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class FormSummator : System.Windows.Forms.Form
+    {
+        public FormSummator()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal firstNum = decimal.Parse(this.textBoxFirstNum.Text);
+                decimal secondNum = decimal.Parse(this.textBoxSecondNum.Text);
+                decimal sum = firstNum + secondNum;
+                this.textBoxSum.Text = sum.ToString();
+            }
+            catch (Exception)
+            {
+                this.textBoxSum.Text = "error";
+            }
+        }
+    }
+}
